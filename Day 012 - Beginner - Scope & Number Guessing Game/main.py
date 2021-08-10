abceb7fcf0b23dc5602e7ece5d66ccdeed2c1c37
zellko <wyttenda@tutanota.com>
2021-08-10 21:48:49 +0200

    GuessTheNumber

--- Day 012 - Beginner - Scope & Number Guessing Game/main.py
+++ Day 012 - Beginner - Scope & Number Guessing Game/main.py
@@ -1,20 +1,42 @@
 import random
 from art import logo
+
 print(logo)
-print("""
-Welcome to the Number Guessing Game!
-I'm thinking of a number between 1 and 100.
-""")
+print("""Welcome to the Number Guessing Game!
+I'm thinking of a number between 1 and 100.""")
 
 RANDOM_NUMBER = random.randint(1, 100)
 print(f"Pssst, the correct answer is {RANDOM_NUMBER}")
 
+if input("Choose a difficulty. Type 'easy' or 'hard': ").lower() == "easy":
+    NBR_OF_GUESS = 10
+else:
+    NBR_OF_GUESS = 5
+
+END_OF_THE_GAME = False
 
 
+def check_user_guess(guess):
+    user_won = False
+    if guess == RANDOM_NUMBER:
+        print(f"You got it! The answer was {RANDOM_NUMBER}.")
+        user_won = True
+    elif guess > RANDOM_NUMBER:
+        print("Too high\nGuess again")
+    elif guess < RANDOM_NUMBER:
+        print("Too low\nGuess again")
+    return user_won
 
 
+while not END_OF_THE_GAME:
+    user_guess = int(input(f"""You have {NBR_OF_GUESS} attempts remaining to guess the number.
+Make a guess:"""))
+    if check_user_guess(user_guess) or NBR_OF_GUESS == 1:
+        END_OF_THE_GAME = True
+    else:
+        NBR_OF_GUESS -= 1
 
-#Number Guessing Game Objectives:
+# Number Guessing Game Objectives:
 
 # Include an ASCII art logo.
 # Allow the player to submit a guess for a number between 1 and 100.
@@ -23,4 +45,3 @@ print(f"Pssst, the correct answer is {RANDOM_NUMBER}")
 # Track the number of turns remaining.
 # If they run out of turns, provide feedback to the player.
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
-
