228cce49b582db20d7e9e5596f89fe6597a11fdc
zellko <wyttenda@tutanota.com>
2021-08-12 20:33:12 +0200

    /

---
+++ Day 017 -/question_model.py
@@ -0,0 +1,14 @@
+class Question:
+
+    def __init__(self, text, answer):
+        #Initialise attributes
+        #Below 2 parameters are mandatory and need to be specified when the user is created
+        self.text = text # Text of the question
+        self.answer = answer # Answer of the question
+
+
+
+"""
+new_q = Question("bla bla", "oui")
+print(new_q.text)
+"""
