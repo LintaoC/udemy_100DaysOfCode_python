28bb51d2066c368495e55664045f3981d361757b
zellko <wyttenda@tutanota.com>
2021-08-20 22:32:35 +0200

    Automated Birthday Wisher

---
+++ Day 032 - Intermediate Send Email (smtplib) & Manage Dates (datetime)/day 032 - start/main.py
@@ -0,0 +1,26 @@
+import smtplib
+import datetime as dt
+import random
+
+my_email = "pythonbot@gmail.com"
+password = "password
+
+with open("quotes.txt") as file:
+    quote_list = file.readlines()
+
+quote_random = quote_list[random.randint(0, 101)]
+
+datetime_now = dt.datetime.now()
+day_of_week = datetime_now.weekday()
+
+if day_of_week == 4:
+    print("email send")
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=f"Subject:This is the subject\n\nHi!"
+                f"\nToday is Friday, the {day_of_week+1} day of the week.\n"
+                f"There is you motivation quote: \n{quote_random}\nBye")
---
+++ Day 032 - Intermediate Send Email (smtplib) & Manage Dates (datetime)/main.py
@@ -0,0 +1,35 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+my_email = "pythonbot@gmail.com"
+password = "password"
+
+birthday_dataframe = pandas.read_csv("birthdays.csv")
+birthday_dict = birthday_dataframe.to_dict("records")
+
+datetime_now = dt.datetime.now()
+date_now_list = [datetime_now.month, datetime_now.day]
+
+
+def prepare_letter(email, name):
+    with open(f"letter_templates/letter_{random.randint(1, 3)}.txt") as letter:
+        letter_template = letter.read()
+    letter_ready = letter_template.replace("[NAME]", name)
+    send_email(email, name, letter_ready)
+
+
+def send_email(email, name, letter):
+    print("email send")
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=email,
+            msg=f"Subject:HAPPY BIRTHDAY!\n\n{letter}")
+
+
+[prepare_letter(email=row["email"], name=row["name"])
+ for row in birthday_dict if date_now_list == [row["month"], row["day"]]]
