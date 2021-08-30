8bbfad456499060add0b6bbe6bafb54859ae7583
zellko <wyttenda@tutanota.com>
2021-08-30 21:50:55 +0200

    Spotify Time Machine

--- Day 046/main.py
+++ Day 046/main.py
@@ -9,75 +9,67 @@ SPOTIFY_SECRET = os.environ.get("SPOTIFY_SECRET")
 SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME")
 PLAYLIST_NAME = "Time Machine"
 
-# ************ CREATION OF THE PLAYLIST *************
 
 def create_playlist():
+    """CREATION OF THE PLAYLIST"""
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID,
                                                    client_secret=SPOTIFY_SECRET,
-                                                    redirect_uri="http://127.0.0.1:9090",
-                                                    scope="playlist-modify-private"))
-    
-    
-    sp.user_playlist_create(SPOTIFY_USERNAME, PLAYLIST_NAME, False, False, description="UDEMY Day 046 Spotify Time Machine")
+                                                   redirect_uri="http://127.0.0.1:9090",
+                                                   scope="playlist-modify-private"))
 
-# ************ GET ALL MY PLAYLIST (AND CHECK IF THE PLAYLIST "Time Machine" EXIST) *************
+    sp.user_playlist_create(SPOTIFY_USERNAME, PLAYLIST_NAME, False, False,
+                            description="UDEMY Day 046 Spotify Time Machine")
+
 
 def check_for_playlist(name):
+    """GET ALL MY PLAYLIST (AND CHECK IF THE PLAYLIST "Time Machine" EXIST)"""
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID,
                                                    client_secret=SPOTIFY_SECRET,
                                                    redirect_uri="http://127.0.0.1:9090",
                                                    scope="playlist-read-private"))
-    
+
     my_playlists = sp.user_playlists(SPOTIFY_USERNAME)
-    
+
     for items in my_playlists["items"]:
         if name == items["name"]:
             print(f"Playlist ID: {items['id']}")
-            return True
+            return True, items['id']
         else:
             return False
-        
-# ************ get the song ID from a song title + year *************        
-def get_song_list_uri():
+
+
+def get_song_list_uri(track_name, track_year):
+    """get the song ID from a song title + year"""
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID,
                                                    client_secret=SPOTIFY_SECRET,
                                                    redirect_uri="http://127.0.0.1:9090",
                                                    scope="user-read-private"))
-        
-    search_result = sp.search(q="one more time year:2001", type="track")
-    print(search_result["tracks"]["items"][0]["uri"])
-    
 
-# ************ Add a list of track to the playlist *************        
-def add_track_to_playlist():
+    search_result = sp.search(q=f"{track_name} year:{track_year - 1}-{track_year}", type="track")
+    return search_result["tracks"]["items"][0]["uri"]
+
+
+def add_track_to_playlist(spotify_playlist_id, track_list):
+    """Add a list of track to the playlist"""
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID,
                                                    client_secret=SPOTIFY_SECRET,
                                                    redirect_uri="http://127.0.0.1:9090",
                                                    scope="playlist-modify-private"))
-        
-    sp.user_playlist_add_tracks(SPOTIFY_USERNAME, "1JJQpB7iHuAApaeJRVvUQR",["spotify:track:0DiWol3AO6WpXZgp0goxAV"])
-    
 
-# ************ Remove a list of track to the playlist *************        
-def remove_track_to_playlist():
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID,
-                                                   client_secret=SPOTIFY_SECRET,
-                                                   redirect_uri="http://127.0.0.1:9090",
-                                                   scope="playlist-modify-private"))
-        
-    sp.user_playlist_remove_all_occurrences_of_tracks(SPOTIFY_USERNAME, "1JJQpB7iHuAApaeJRVvUQR",["spotify:track:0DiWol3AO6WpXZgp0goxAV"])
+    # Replacing the old playlist with the new one
+    sp.playlist_replace_items(spotify_playlist_id, track_list)
 
 
-# If the "Time Machine" playlist do not exist, we create it. 
-if check_for_playlist(PLAYLIST_NAME):
-    print("No need to create a new playlist")
-else:
+# If the "Time Machine" playlist do not exist, we create it.
+check_playlist = check_for_playlist(PLAYLIST_NAME)
+
+if not check_playlist:
     create_playlist()
+    playlist_id = check_for_playlist(PLAYLIST_NAME)[1]  # Getting the playlist ID
     print("I create the time machine playlist")
-
-get_song_list_uri()
-#add_track_to_playlist()
-remove_track_to_playlist()
+else:
+    playlist_id = check_playlist[1]
+    print("No need to create a new playlist")
 
 year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
@@ -87,7 +79,18 @@ r = requests.get(url)
 billboard_page = r.text
 
 soup = BeautifulSoup(billboard_page, 'html.parser')
-song_titles = soup.find_all("span", class_="chart-element__information__song")
-song_titles_list = [title.getText() for title in song_titles]
+song_tracks = soup.find_all("span", class_="chart-element__information__song")
+song_tracks_list = [title.getText() for title in song_tracks]
 
-print(song_titles_list)
+year = year.split("-")  # Getting the year from the date to refine the URI search
+song_uri_list = []
+
+# ************ Get song URI and adding them to a list *************
+for track in song_tracks_list:
+    try:
+        song_uri_list.append(get_song_list_uri(track, int(year[0])))
+    except IndexError:
+        print("not found")
+
+# ************ Add the tracks to the playlist *************
+add_track_to_playlist(playlist_id, song_uri_list)
