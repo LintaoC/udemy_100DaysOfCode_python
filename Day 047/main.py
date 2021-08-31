f51684a7b2553ff256bea831fbf86c21c4c2c15f
zellko <88422136+zellko@users.noreply.github.com>
2021-08-31 09:00:59 +0200

    Automated Amazon Price Tracker

--- Day 047/main.py
+++ Day 047/main.py
@@ -6,23 +6,21 @@ import smtplib
 my_email = os.environ.get("my_email")
 password = os.environ.get("password")
 
+TARGET_PRICE = 100.0
 
-def send_email():
-    print("email send")
+
+def send_email(item_price, item_name, item_url):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(
             from_addr=my_email,
-            to_addrs=my_email,
-            msg=f"Subject:Amzone Price alert!\n\nCore email")
+            to_addrs="damien.wytttenbach@rdor.nestle.com",
+            msg=f"Subject:Amzone Price alert!\n\n{item_name} is now ${item_price}\n{item_url}")
 
 
 # ************ SCRAPING THE ITEM PRICE *************
-url = "https://www.amazon.com/" \
-      "ASUS-Graphics-Axial-Tech-2-9-Slot-AllyFlex/dp/B092KQ6LGF/" \
-      "ref=sr_1_3?crid=3R38I01TZULMJ&dchild=1&keywords=nvidia+geforce+rtx+3060+ti&qid=" \
-      "1630354093&sprefix=nvida+%2Caps%2C200&sr=8-3"
+url = "https://www.amazon.com/PlayStation-Classic-Console/dp/B07HHVF2XG/ref=sr_1_1?dchild=1&keywords=playstation+consoles&pf_rd_i=23508887011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=434db2ed-6d53-4c59-b173-e8cd550a2e4f&pf_rd_r=4G7T8RYC6THTQ9YV28W4&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1630391583&s=videogames&sr=1-1"
 
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
@@ -32,7 +30,12 @@ r = requests.get(url, headers=header)
 amazon_page = r.text
 
 soup = BeautifulSoup(amazon_page, 'html.parser')
-price = soup.find("span", class_="a-size-medium a-color-price priceBlockBuyingPriceString")
-price_int = int(price.getText()[1:-3].replace(",", ""))  # Refining the price, to turn it into an int
-print(price_int)
-print(type(price_int))
+price = soup.find("span", class_="a-size-base a-color-price")
+price_float = float(price.getText().replace("$", "").replace(",", ""))  # Refining the price, to turn it into an float
+name = soup.find("span", class_="a-size-large product-title-word-break")
+print(name.getText().strip())
+print(price_float)
+
+if price_float <= TARGET_PRICE:
+    print("sending email")
+    send_email(price_float, name, url)
