8b05445b409c990c2da893d51e4c9151426e5c59
zellko <88422136+zellko@users.noreply.github.com>
2021-09-07 15:58:59 +0200

    Update server.py

--- Day 056/name_card/server.py
+++ Day 056/name_card/server.py
@@ -7,9 +7,5 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/cv")
-def cv():
-    return render_template("personal_website.html")
-
 if __name__ == "__main__":
     app.run(debug=False)
